a0cea958e36d26a6885c9910f2cecfc1c77374e1
ISUCON user <hoge@example.com>
2013-10-06 16:15:15 +0900

    monocerosnize

--- env.sh
+++ env.sh
@@ -8,6 +8,6 @@ PERLPATH=$HOME/local/perl-5.18/bin
 RUBYPATH=$HOME/local/ruby-2.0/bin
 PYTHONPATH=$HOME/local/python-3.3/bin
 PATH=$PYTHONPATH:$RUBYPATH:$PERLPATH:$NODEPATH:$GOPATH:$PATH:$HOME/bin
-
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export PATH GOPATH GOROOT
 exec "$@"
