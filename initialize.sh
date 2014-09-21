f8cac22843a108d2061e410da1a2b0d2a5817dbd
chrone <irie-yuki@kayac.com>
2014-09-21 18:01:09 +0900

    run initialize.sh on deploy.sh

--- initialize.sh
+++ initialize.sh
@@ -5,6 +5,13 @@ umask 0077
 export PATH='/bin:/usr/bin'
 export LANG='C'
 
+mysql -B -N -uroot -proot -e'show indexes from memos' isucon | grep i_memos_1
+[[ $? = 0 ]] && mysql -uroot -proot -e'drop index i_memos_1 on memos' isucon
+mysql -B -N -uroot -proot -e'show indexes from memos' isucon | grep i_memos_2
+[[ $? = 0 ]] && mysql -uroot -proot -e'drop index i_memos_2 on memos' isucon
+mysql -B -N -uroot -proot -e'show indexes from memos' isucon | grep i_memos_3
+[[ $? = 0 ]] && mysql -uroot -proot -e'drop index i_memos_3 on memos' isucon
+
 mysql -uroot -proot isucon << _EOT_
 create index i_memos_1 on memos (is_private, created_at);
 create index i_memos_2 on memos (user, created_at);
