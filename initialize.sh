f450cd2b51ff287a8dc724faf7510a9e0994d611
chrone <irie-yuki@kayac.com>
2014-09-21 16:57:28 +0900

    fix index name

--- initialize.sh
+++ initialize.sh
@@ -8,5 +8,5 @@ export LANG='C'
 mysql -uroot -proot isucon << _EOT_
 create index i_memos_1 on memos (is_private, created_at);
 create index i_memos_2 on memos (user, created_at);
-create index i_memos_2 on memos (user, is_private, created_at);
+create index i_memos_3 on memos (user, is_private, created_at);
 _EOT_
